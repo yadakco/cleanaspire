774ef06ddf851edd691e20330a2fbb3a99ab23a0
hualin.zhu <neo.js.cn@gmail.com>
2024-11-20 18:33:22 +0800

    remove MailKit

--- src/CleanAspire.Api/Identity/EmailSender.cs
+++ src/CleanAspire.Api/Identity/EmailSender.cs
@@ -2,66 +2,35 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using MailKit.Net.Smtp;
+
 using Microsoft.AspNetCore.Identity.UI.Services;
-using MimeKit;
+using StrongGrid;
+
 
 namespace CleanAspire.Api.Identity;
 
 public class EmailSender : IEmailSender
 {
 
-    private readonly SmtpClientOptions _smtpOptions;
+    private readonly string _sendGridApiKey;
+    private readonly string _defaultFromEmail;
     public EmailSender(IConfiguration configuration)
     {
-        _smtpOptions = configuration.GetSection("SmtpClientOptions").Get<SmtpClientOptions>();
+        _sendGridApiKey = configuration["SendGrid:ApiKey"]?? "";
+        _defaultFromEmail = configuration["SendGrid:DefaultFromEmail"] ?? "noreply@blazorserver.com";
     }
     public async Task SendEmailAsync(string email, string subject, string htmlMessage)
     {
-        var message = new MimeMessage();
-        message.From.Add(new MailboxAddress("", _smtpOptions.DefaultFromEmail));
-        message.To.Add(new MailboxAddress("", email));
-        message.Subject = subject;
-
-        var bodyBuilder = new BodyBuilder
+        if (string.IsNullOrEmpty(_sendGridApiKey))
         {
-            HtmlBody = htmlMessage
-        };
-        message.Body = bodyBuilder.ToMessageBody();
-
-        using var smtpClient = new SmtpClient();
-        if (_smtpOptions.UsePickupDirectory && !string.IsNullOrEmpty(_smtpOptions.MailPickupDirectory))
-        {
-            smtpClient.LocalDomain = "localhost";
-            await smtpClient.SendAsync(message);
+            throw new InvalidOperationException("SendGrid API Key is not configured.");
         }
-        else
-        {
-            await smtpClient.ConnectAsync(_smtpOptions.Server, _smtpOptions.Port, _smtpOptions.UseSsl);
 
-            if (_smtpOptions.RequiresAuthentication)
-            {
-                await smtpClient.AuthenticateAsync(_smtpOptions.User, _smtpOptions.Password);
-            }
-
-            await smtpClient.SendAsync(message);
-            await smtpClient.DisconnectAsync(true);
-        }
+        var client = new Client(_sendGridApiKey);
+        var from = new StrongGrid.Models.MailAddress(_defaultFromEmail, "noreply");
+        var to = new StrongGrid.Models.MailAddress(email,email);
+        var messageId = await client.Mail.SendToSingleRecipientAsync(to:to,from:from, subject:subject, htmlContent:htmlMessage,textContent: htmlMessage);
     }
 }
 
-public class SmtpClientOptions
-{
-    public const string Key = nameof(SmtpClientOptions);
-    public string Server { get; set; }
-    public int Port { get; set; } = 25;
-    public string User { get; set; }
-    public string Password { get; set; }
-    public bool UseSsl { get; set; } = false;
-    public bool RequiresAuthentication { get; set; } = true;
-    public string PreferredEncoding { get; set; }
-    public bool UsePickupDirectory { get; set; } = false;
-    public string MailPickupDirectory { get; set; }
-    public object SocketOptions { get; set; }
-    public string DefaultFromEmail { get; set; }
-}
+ 
