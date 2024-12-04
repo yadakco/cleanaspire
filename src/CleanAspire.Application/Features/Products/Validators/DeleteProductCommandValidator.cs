f938ea470d901877dada5f1c45dd8c14048c7d38
hualin <hualin1.zhu@voith.com>
2024-12-04 17:31:33 +0800

    commit

---
+++ src/CleanAspire.Application/Features/Products/Validators/DeleteProductCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanAspire.Application.Features.Products.Commands;
+
+namespace CleanAspire.Application.Features.Products.Validators;
+public class DeleteProductCommandValidator : AbstractValidator<DeleteProductCommand>
+{
+    public DeleteProductCommandValidator()
+    {
+        RuleFor(command => command.Ids)
+            .NotEmpty().WithMessage("At least one product ID is required.")
+            .Must(ids => ids != null && ids.All(id => !string.IsNullOrWhiteSpace(id))).WithMessage("Product IDs must not be empty or whitespace.");
+    }
+}
---
+++ src/CleanAspire.Application/Pipeline/ValidationPreProcessor.cs
@@ -0,0 +1,35 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediator;
+
+namespace CleanAspire.Application.Pipeline;
+public class ValidationPreProcessor<TRequest,TResponse> : MessagePreProcessor<TRequest, TResponse>
+{
+    private readonly IValidator<TRequest>? _validator;
+
+    public ValidationPreProcessor(IValidator<TRequest>? validator = null)
+    {
+        _validator = validator;
+    }
+
+    public async ValueTask<TResponse> ProcessAsync(TRequest request, CancellationToken cancellationToken)
+    {
+        if (_validator != null)
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResult = await _validator.ValidateAsync(context, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                throw new ValidationException(validationResult.Errors);
+            }
+        }
+    }
+}
