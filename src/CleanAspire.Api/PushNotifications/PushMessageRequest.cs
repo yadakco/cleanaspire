ca07afa906c421a369b4e08cc2141b6194ad0430
hualin.zhu <neo.js.cn@gmail.com>
2024-12-07 19:41:19 +0800

    Create PushMessageRequest.cs

---
+++ src/CleanAspire.Api/PushNotifications/PushMessageRequest.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Lib.Net.Http.WebPush;
+
+namespace CleanAspire.Api.PushNotifications;
+
+public class PushMessageRequest
+{
+    public required string Topic { get; set; }
+
+    public required string Notification { get; set; }
+
+    public PushMessageUrgency Urgency { get; set; } = PushMessageUrgency.Normal;
+}
