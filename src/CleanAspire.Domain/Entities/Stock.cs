485b2a7da36063c13586ac0f0d74d42d0cc7b6dd
hualin.zhu <neo.js.cn@gmail.com>
2025-01-06 20:31:53 +0800

    create StockConfiguration

--- src/CleanAspire.Domain/Entities/Stock.cs
+++ src/CleanAspire.Domain/Entities/Stock.cs
@@ -10,10 +10,29 @@ using System.Threading.Tasks;
 using CleanAspire.Domain.Common;
 
 namespace CleanAspire.Domain.Entities;
+
+/// <summary>
+/// Represents a stock entity.
+/// </summary>
 public class Stock : BaseAuditableEntity, IAuditTrial
 {
+    /// <summary>
+    /// Gets or sets the product ID.
+    /// </summary>
     public string? ProductId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the product associated with the stock.
+    /// </summary>
     public Product? Product { get; set; }
+
+    /// <summary>
+    /// Gets or sets the quantity of the stock.
+    /// </summary>
     public int Quantity { get; set; }
-    public string Location { get; set; }=string.Empty;
+
+    /// <summary>
+    /// Gets or sets the location of the stock.
+    /// </summary>
+    public string Location { get; set; } = string.Empty;
 }
