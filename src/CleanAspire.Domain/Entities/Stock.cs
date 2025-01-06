82fa5d4f9f5738c14d0cc4fb5d8503ef2a5d412b
hualin.zhu <neo.js.cn@gmail.com>
2025-01-06 20:26:04 +0800

    Step 1: Add the Stock Entity in the CleanAspire.Domain Project

---
+++ src/CleanAspire.Domain/Entities/Stock.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanAspire.Domain.Common;
+
+namespace CleanAspire.Domain.Entities;
+public class Stock : BaseAuditableEntity, IAuditTrial
+{
+    public string? ProductId { get; set; }
+    public Product? Product { get; set; }
+    public int Quantity { get; set; }
+    public string Location { get; set; }=string.Empty;
+}
