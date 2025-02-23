45c139ad66ba7888cf267b775b0ab922e5197f41
hualin.zhu <neo.js.cn@gmail.com>
2025-02-23 16:01:58 +0800

    done

---
+++ src/CleanAspire.Infrastructure/Configurations/MinioOptions.cs
@@ -0,0 +1,19 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanAspire.Infrastructure.Configurations;
+public class MinioOptions
+{
+    public const string Key = "Minio";
+    public string Endpoint { get; set; } = "minio.blazorserver.com";
+    public string AccessKey { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public string BucketName { get; set; } = "demo";
+}
